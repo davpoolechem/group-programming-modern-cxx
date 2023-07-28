de52305f72eacf7a84e67758992668936e128a4f
david poole <dpoole34@gatech.edu>
2023-07-28 09:16:15 -0400

    Add has_value() check to optional.cpp

--- src/optional/optional.cpp
+++ src/optional/optional.cpp
@@ -3,12 +3,12 @@
 #include <optional>
 
 std::optional<double> mysqrt(double input) {
-  return input >= 0.0 ? std::optional<double>(sqrt(input)) : std::nullopt;
+  return input >= 0.0 ? std::make_optional<double>(sqrt(input)) : std::nullopt;
 }
 
 int main(int argc, char *argv[]) {
   std::optional<double> result = mysqrt(std::atof(argv[1]));
 
-  std::cout << result.has_value() << std::endl;
+  std::cout << (result.has_value() ? "Yes" : "No") << std::endl;
   std::cout << result.value() << std::endl;
 }
