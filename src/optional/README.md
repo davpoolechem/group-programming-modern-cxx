d26889ebe424583020c8a11c8a29ae49548b3897
david poole <dpoole34@gatech.edu>
2023-07-28 15:08:50 -0400

    Refine optional demo

--- src/optional/README.md
+++ src/optional/README.md
@@ -3,7 +3,6 @@ Demo of `std::optional` feature, using safer computation of square roots
 
 # File list:
 - optional.cpp: Safer `sqrt` impl using `std::optional` 
-- nooptional.cpp: Regular `sqrt` impl 
 
 # Makefile
 - `make exe` builds executable binaries.
--- src/optional/optional.cpp
+++ src/optional/optional.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 #include <optional>
 
-std::optional<double> mysqrt(double input) {
+//-- plain old square root --// 
+double mysqrt_base(double input) { return sqrt(input); }
+
+//-- safe square root, returning only if a positive number is input --//
+std::optional<double> mysqrt_safe(double input) {
   return input >= 0.0 ? std::make_optional<double>(sqrt(input)) : std::nullopt;
 }
 
 int main(int argc, char *argv[]) {
-  std::optional<double> result = mysqrt(std::atof(argv[1]));
+  auto result_base = mysqrt_base(std::atof(argv[1]));
+  std::cout << "Base value: " << result_base << std::endl;
 
-  std::cout << (result.has_value() ? "Yes" : "No") << std::endl;
-  std::cout << result.value() << std::endl;
+  auto result_safe = mysqrt_safe(std::atof(argv[1]));
+  std::cout << "Safe result has value? " << (result_safe.has_value() ? "Yes" : "No") << std::endl;
+  std::cout << result_safe.value() << std::endl;
 }
